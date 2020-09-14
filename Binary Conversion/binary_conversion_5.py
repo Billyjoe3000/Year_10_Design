a36a21ff19e8004a6936390369b6d1fda18c42f0
Alan Cui <alan.cui@GCCYJPWPJ1WK.local>
2020-09-14 10:26:06 -0400

    binary conversion done

--- Binary Conversion/binary_conversion_5.py
+++ Binary Conversion/binary_conversion_5.py
@@ -1,31 +1,31 @@
 import tkinter as tk
 
-print("Stage 3")
+print("Stage 5")
+
+#############################################################
 
 # conversion gets the value entered
 def conversion(*args):
-	print("conversion function")
 
 	val = ent_value.get()
-	print(val)
 
 	# calling function check01
 	check = check01(val)
-	print(check)
 
 	if check == True:
-		val = removezeros(val)
-		print("Val is binary: " + val) 
+		val = removezeros(val) # passing val to removezeros function
+		result = base2to10(val) # passing val with zeros removed to base2to10 function
+		lab_results.config(text = str(val) + " ==> " + str(result))
 	else:
-		print("Val is not binary")
 		lab_results.config(text = "INVALID INPUT") # updating the lab results widget
 
 
 	ent_value.delete(0,tk.END)
 
+#############################################################
+
 # checks if it's only 0s and 1s
 def check01(str):
-	print("check01 function") # printing check
 
 # counting the number of 0s and 1s
 	num_0 = str.count("0") 
@@ -34,15 +34,34 @@ def check01(str):
 # checking if they add up to the length
 	if num_0 + num_1 == len(str):
 		return True
+
 	return False
+
+#############################################################
 
 #function removes front zeros from input val
 def removezeros(str):
 	for i in range(0, len(str),1):
 		if str[i] == "1":
 			return str[i:]
+
 	return str
+
+#############################################################
  
+def base2to10(str):
+	n = 0
+	total = 0
+
+	# calculating the conversion from back of str to front of str
+	for i in range(len(str) - 1, -1, -1):
+		total = total + int(str[i]) * 2**n
+		n = n + 1
+
+	return total
+
+#############################################################
+
 # Create root window
 root = tk.Tk()
 
@@ -59,3 +78,4 @@ lab_results.pack()
 # Binding the return key to the function called conversion
 root.bind("<Return>", conversion)
 root.mainloop()
+
